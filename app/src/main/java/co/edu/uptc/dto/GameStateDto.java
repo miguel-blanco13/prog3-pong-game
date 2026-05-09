7cc8251e72ad1d7744924f908a5d5ced14b3d74c
Miguel Blanco <blancoperillaj@gmail.com>
2026-05-09 16:01:33 -0500

    refactor: collapse to single Maven module, remove Lombok

--- app/src/main/java/co/edu/uptc/dto/GameStateDto.java
+++ app/src/main/java/co/edu/uptc/dto/GameStateDto.java
@@ -2,12 +2,11 @@ package co.edu.uptc.dto;
 
 import co.edu.uptc.pojo.Ball;
 import co.edu.uptc.pojo.Paddle;
-import lombok.Data;
 
 import java.util.List;
 
-@Data
 public class GameStateDto {
+
     private List<Ball> balls;
     private Paddle     paddle;
     private int        score;
@@ -18,4 +17,26 @@ public class GameStateDto {
     private double     speedMultiplier;
     private long       startTimeMs;
     private long       elapsedMs;
+
+    public List<Ball> getBalls()           { return balls; }
+    public Paddle     getPaddle()          { return paddle; }
+    public int        getScore()           { return score; }
+    public boolean    isGameOver()         { return gameOver; }
+    public boolean    isPaused()           { return paused; }
+    public boolean    isDarkMode()         { return darkMode; }
+    public boolean    isCircleBall()       { return circleBall; }
+    public double     getSpeedMultiplier() { return speedMultiplier; }
+    public long       getStartTimeMs()     { return startTimeMs; }
+    public long       getElapsedMs()       { return elapsedMs; }
+
+    public void setBalls(List<Ball> balls)              { this.balls = balls; }
+    public void setPaddle(Paddle paddle)                { this.paddle = paddle; }
+    public void setScore(int score)                     { this.score = score; }
+    public void setGameOver(boolean gameOver)           { this.gameOver = gameOver; }
+    public void setPaused(boolean paused)               { this.paused = paused; }
+    public void setDarkMode(boolean darkMode)           { this.darkMode = darkMode; }
+    public void setCircleBall(boolean circleBall)       { this.circleBall = circleBall; }
+    public void setSpeedMultiplier(double sm)           { this.speedMultiplier = sm; }
+    public void setStartTimeMs(long startTimeMs)        { this.startTimeMs = startTimeMs; }
+    public void setElapsedMs(long elapsedMs)            { this.elapsedMs = elapsedMs; }
 }
--- app/src/main/java/co/edu/uptc/pojo/Ball.java
+++ app/src/main/java/co/edu/uptc/pojo/Ball.java
@@ -1,14 +1,39 @@
 package co.edu.uptc.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data @NoArgsConstructor @AllArgsConstructor
 public class Ball {
+
     private double x, y;
     private double dx, dy;
     private double size;
     private boolean circle;
     private int bounceCount;
+
+    public Ball() {}
+
+    public Ball(double x, double y, double dx, double dy,
+                double size, boolean circle, int bounceCount) {
+        this.x           = x;
+        this.y           = y;
+        this.dx          = dx;
+        this.dy          = dy;
+        this.size        = size;
+        this.circle      = circle;
+        this.bounceCount = bounceCount;
+    }
+
+    public double  getX()           { return x; }
+    public double  getY()           { return y; }
+    public double  getDx()          { return dx; }
+    public double  getDy()          { return dy; }
+    public double  getSize()        { return size; }
+    public boolean isCircle()       { return circle; }
+    public int     getBounceCount() { return bounceCount; }
+
+    public void setX(double x)            { this.x = x; }
+    public void setY(double y)            { this.y = y; }
+    public void setDx(double dx)          { this.dx = dx; }
+    public void setDy(double dy)          { this.dy = dy; }
+    public void setSize(double size)      { this.size = size; }
+    public void setCircle(boolean circle) { this.circle = circle; }
+    public void setBounceCount(int n)     { this.bounceCount = n; }
 }
